57c74144f089a41e05cebb5db940c9eb3822a445
Jana Jarecki <jarecki.jana@gmail.com>
2022-07-12 18:58:16 +0200

    adds day 2 material

--- 1-Lectures/data/make-ggplot-data.R
+++ 1-Lectures/data/make-ggplot-data.R
@@ -1,4 +1,5 @@
 library(data.table)
+setwd(dir=dirname(rstudioapi::getActiveDocumentContext()$path))
 set.seed(42)
 N <- 40
 cond <- 1:2
@@ -10,15 +11,15 @@ d[, judgment := rnorm(.N, cond, 3)]
 d[, certainty := 0.2 + 0.2 * judgment + 1.2 * condition + rnorm(.N, 0, 1) ]
 d[, choice := rbinom(.N, 1, prob=1/condition-.2)]
 
+d[, model_X_pred := runif(.N,.1,.5) * 0^(choice) + runif(.N,.5,.9) * 0^(1-choice)]
+d[, model_Y_pred := runif(.N,.4,.7) * 0^(choice) + runif(.N,.1,.7) * 0^(1-choice)]
+
 d[, model_X_evidence := rbeta(N, 1, 5)]
 d[, model_Y_evidence := 1 - model_X_evidence]
 d[, alpha := rnorm(.N, 0.4, .1)]
 d[, delta := runif(.N, 0, 5)]
-d[, model_X_pred := runif(.N,.1,.5) * 0^(choice) + runif(.N,.3,.8) * 0^(1-choice)]
-d[, model_Y_pred := runif(.N,.4,.8) * 0^(choice) + runif(.N,.1,.7) * 0^(1-choice)]
-d[, mean(model_X_pred), by=choice]
 
 
 d[, condition := factor(condition, labels = c("A","B"))]
 
-fwrite(d, "data/data_ggplot.csv")
+fwrite(d, "ggplot-data.csv")
